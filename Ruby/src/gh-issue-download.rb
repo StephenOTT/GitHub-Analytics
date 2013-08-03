73a6acbd3de7789d6517b0457b8ff8e0b711b067
Stephen Russett <stephen.russett@ottawa.ca>
2013-08-03 16:32:57 -0400

    Added initial mongodb support + code touchups
    Code imports all issues (no comments).  Added the Auto-Pagination
    feature that auto-increments through each page of issues.  Removed
    JSON.Pretty_generate because it is not needed for mongodb import

--- Ruby/src/gh-issue-download.rb
+++ Ruby/src/gh-issue-download.rb
@@ -1,14 +1,23 @@
 require 'octokit'
 require 'json'
+require 'mongo'
 
+include Mongo
 
 
 class IssueDownload
 
+
 	def initialize (repository)
 		
 		@repository = repository
-				
+		
+		# MongoDB Database Connect
+		@client = MongoClient.new('localhost', 27017)
+		@db = @client['test']
+		@coll = @db['githubIssues']
+		@coll.remove		
+	
 	end
 	
 	def ghAuthenticate
@@ -20,19 +29,36 @@ class IssueDownload
 		password = gets
 		
 		
-		@ghclient = Octokit::Client.new(:login => username.to_s, :password => password.to_s)		
+		#@ghclient = Octokit::Client.new(:login => username.to_s, :password => password.to_s)		
 	end
 	
 	
 	def getIssues
 		
+		# Auto-Pagination feature that increments through all pages of issues:
+		# See: http://rdoc.info/gems/octokit/Octokit/Configuration#DEFAULT_AUTO_TRAVERSAL-constant
+		# See:https://github.com/octokit/octokit.rb/pull/64 
+		Octokit.configure do |c|
+  		c.auto_traversal = true
+		end
+		
+		
 		issueResults = Octokit.list_issues (@repository.to_s)
-		return JSON.pretty_generate(issueResults.first)
+		#return JSON.pretty_generate(issueResults.first)
+		return issueResults
+	end
+	
+	
+	def putIntoMongo
+		@coll.insert(getIssues)
+		
+	
 	end
 	
 
 
 end
 
-start = IssueDownload.new("CityofOttawa/Ottawa-ckan")
-puts start.getIssues
+start = IssueDownload.new("wet-boew/wet-boew")
+#puts start.getIssues
+start.putIntoMongo
