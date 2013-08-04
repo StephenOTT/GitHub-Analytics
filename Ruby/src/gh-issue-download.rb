e1564cce1734dec1694790ec84e3ff5069abf0d7
Stephen Russett <stephen.russett@ottawa.ca>
2013-08-04 00:12:25 -0400

    Authentication added accross the board + MongoCollection comments merge
    Added authentication to all requests because of the unauthenticated
    rate limit.  Also refactored the mongo code to push comments into the
    same collections as the issues.  Issues and comments are now part of
    the same collection.  See lines 81 to 83/The IssuesWithComments loop.

--- Ruby/src/gh-issue-download.rb
+++ Ruby/src/gh-issue-download.rb
@@ -7,7 +7,6 @@ include Mongo
 
 class IssueDownload
 
-
 	def initialize (repository)
 		
 		@repository = repository
@@ -15,54 +14,39 @@ class IssueDownload
 		# MongoDB Database Connect
 		@client = MongoClient.new('localhost', 27017)
 		@db = @client['test']
+		
 		@coll = @db['githubIssues']
 		@coll.remove
 		
 		@collComments = @db['githubComments']
-		@collComments.remove		
-	
+		@collComments.remove	
 	end
 	
 	# TODO add authentication as a option for go live as Github Rate Limit is 60 hits per hour when unauthenticated by 5000 per hour when authenticated.
 	def ghAuthenticate
 		puts "Enter GitHub Username:"
 		username = gets
-		
+
 		# TODO Add Highline gem support for Password no-Echo
 		puts "Enter GitHub Password:"
 		password = gets
-		
-		
-		#@ghclient = Octokit::Client.new(:login => username.to_s, :password => password.to_s)		
+		@ghClient = Octokit::Client.new(:login => username.to_s, :password => password.to_s, :auto_traversal => true)		
 	end
 	
-	
-	def ghConfigAutoTraversal
-	
-		# Auto-Pagination feature that increments through all pages of issues:
-		# See: http://rdoc.info/gems/octokit/Octokit/Configuration#DEFAULT_AUTO_TRAVERSAL-constant
-		# See:https://github.com/octokit/octokit.rb/pull/64 
-		Octokit.configure do |c|
-  		c.auto_traversal = true
-		end
 		
-	
-	end
-	
-	
 	def getIssues
-		
-		self.ghConfigAutoTraversal
-		
+				
 		# TODO get list_issues working with options hash: Specifically need Open and Closed issued to be captured
-		issueResults = Octokit.list_issues (@repository.to_s)
+		issueResults = @ghClient.list_issues (@repository.to_s)
 		#return JSON.pretty_generate(issueResults.first) 
+		puts "Github raite limit remaining: " + @ghClient.ratelimit_remaining.to_s
 		return issueResults
 	end
 	
 	
 	def putIntoMongo
 		@coll.insert(getIssues)
+		puts "Issues added to Mongodb: " + @coll.count.to_s
 	
 	end
 	
@@ -70,17 +54,24 @@ class IssueDownload
 	# find records in Mongodb that have a comments field value of 1 or higher
 	# returns only the number field
 	def findIssuesWithComments
-	
-		self.ghConfigAutoTraversal
-	
+		
 		#find filter is based on: http://stackoverflow.com/a/10443659
 		issuesWithComments = @coll.find({"comments" => {"$gt" => 0}}, {:fields => {"_id" => 0, "number" => 1}}).to_a
 		
 		issuesWithComments.each do |x|
  			 puts x["number"]
- 			 issueComments = Octokit.issue_comments(@repository.to_s, x["number"].to_s)
- 			 @collComments.insert(issueComments)
+ 			 issueComments = @ghClient.issue_comments(@repository.to_s, x["number"].to_s)
+ 			 puts issueComments
+ 			 
+			 @coll.update(
+			 	{ "number" => x["number"]},
+			 	{ "$push" => {"comments_Text" => issueComments}}
+			 	)
+
+
 		end 
+		puts "Github raite limit remaining: " + @ghClient.ratelimit_remaining.to_s
+		puts "Comments added to Mongodb: " + @collComments.count.to_s
 				
 
 end
@@ -92,5 +83,9 @@ end
 start = IssueDownload.new("wet-boew/wet-boew-drupal")
 #start = IssueDownload.new("StephenOTT/Test1")
 #puts start.getIssues
+start.ghAuthenticate
 start.putIntoMongo
 start.findIssuesWithComments
+
+
+
