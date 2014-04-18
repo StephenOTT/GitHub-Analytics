94cf86ccdf0d4ead7e82e5e94e410b6405467c8a
Stephen Russett <stephenrussett@gmail.com>
2014-04-17 23:01:17 -0400

    New helpers for download and first round of analysis

--- app/sinatra_helpers.rb
+++ app/sinatra_helpers.rb
@@ -1,8 +1,17 @@
-
+require_relative '../github-analytics-data-download/controller'
+require_relative '../github-analytics-analyze-data/issues_processor'
 
 module Sinatra_Helpers
 
-    
+    def self.download_github_analytics_data(user, repo, githubObject, githubAuthInfo)
+      userRepo = "#{user}/#{repo}" 
+      Analytics_Download_Controller.controller(userRepo, githubObject, true, githubAuthInfo)
+    end
+
+    def self.analyze_issues_opened_per_user(user, repo, githubAuthInfo)
+      userRepo = "#{user}/#{repo}" 
+      Issues_Processor.analyze_issues_opened_per_user(userRepo, githubAuthInfo)
+    end
 
 
 end
