edccd32bf6a5d410f66a2071c8b13508e2f75811
Stephen Russett <stephenrussett@gmail.com>
2014-04-19 01:50:44 -0400

    Commeted out likely unneeded code

--- github-analytics-analyze-data/helpers.rb
+++ github-analytics-analyze-data/helpers.rb
@@ -1,37 +1,37 @@
-require 'chronic_duration'
+# require 'chronic_duration'
 
 module Helpers
 
-	def self.budget_left?(large, small)
-		large - small
-	end
+	# def self.budget_left?(large, small)
+	# 	large - small
+	# end
 
-	def self.convertSecondsToDurationFormat(timeInSeconds, outputFormat)
-		outputFormat = outputFormat.to_sym
-		return ChronicDuration.output(timeInSeconds, :format => outputFormat, :keep_zero => true)
-	end
+	# def self.convertSecondsToDurationFormat(timeInSeconds, outputFormat)
+	# 	outputFormat = outputFormat.to_sym
+	# 	return ChronicDuration.output(timeInSeconds, :format => outputFormat, :keep_zero => true)
+	# end
 
 
-	def self.merge_issue_time_and_budget(issuesTime, issuesBudget)
+	# def self.merge_issue_time_and_budget(issuesTime, issuesBudget)
 
-		issuesTime.each do |t|
+	# 	issuesTime.each do |t|
 
-			issuesBudget.each do |b|
+	# 		issuesBudget.each do |b|
 
-				if b["issue_number"] == t["issue_number"]
-					t["budget_duration_sum"] = b["budget_duration_sum"]
-					t["budget_comment_count"] = b["budget_comment_count"]
-					break
-				end					
-			end
-			if t.has_key?("budget_duration_sum") == false and t.has_key?("budget_comment_count") == false
-				t["budget_duration_sum"] = nil
-				t["budget_comment_count"] = nil
-			end
-		end
+	# 			if b["issue_number"] == t["issue_number"]
+	# 				t["budget_duration_sum"] = b["budget_duration_sum"]
+	# 				t["budget_comment_count"] = b["budget_comment_count"]
+	# 				break
+	# 			end					
+	# 		end
+	# 		if t.has_key?("budget_duration_sum") == false and t.has_key?("budget_comment_count") == false
+	# 			t["budget_duration_sum"] = nil
+	# 			t["budget_comment_count"] = nil
+	# 		end
+	# 	end
 
-		return issuesTime
-	end
+	# 	return issuesTime
+	# end
 
 
 
