1fc6754003106362201064435fcf554a33383039
Stephen Russett <stephenrussett@gmail.com>
2014-04-21 03:24:26 -0400

    Beginning of future support for getting missing dates in a more DRY way.  Still needs work

--- github-analytics-analyze-data/helpers.rb
+++ github-analytics-analyze-data/helpers.rb
@@ -2,6 +2,72 @@
 
 module Helpers
 
+
+
+def self.get_date_formatter(dateGroup)
+
+	case dateGroup
+		when :month
+			return "%b %Y" # Month Year
+		when :week
+			return "Week %U, %Y" # Week Year
+		when :day
+
+	end
+
+end
+
+
+def self.get_missing_dates(repo, output, dateFormater)
+
+	# dateFormater = "%b %Y" # Month Year
+	# dateFormater = "Week %U, %Y" # Week Year
+
+	
+	# Add all dates into a array and sort from oldest to newest
+	a = []
+	output.each do |x|
+		a << x["converted_date"]
+	end
+	a.sort!
+
+	# Create a array of dates based on the date at the start of the "a" array and the last item int eh "a" array
+	b = (output.first["converted_date"]..output.last["converted_date"]).to_a
+	
+	# remove dates that are the same in both "a" and "b" array, then remove duplicate values - output is array of strings
+	zeroValueDates = (b.map{ |date| date.strftime(dateFormater) } - a.map{ |date| date.strftime(dateFormater) }).uniq
+
+	# Iterates through each zeroValueDates array of Strings and parses back into a date and adds to the output array
+	zeroValueDates.each do |zvd|
+		zvd = DateTime.parse(zvd)
+		output << {"repo"=> repo , "created_year"=>zvd.strftime("%Y").to_i, "created_month"=>zvd.strftime("%m").to_i, "count"=>0, "converted_date"=>zvd}
+	end
+
+	return output
+
+end
+
+
+def self.sort_dates_array_hash(arrayofHashes, sortField)
+
+	arrayofHashes = arrayofHashes.sort_by { |hsh| hsh[sortField] }
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	# def self.budget_left?(large, small)
 	# 	large - small
 	# end
