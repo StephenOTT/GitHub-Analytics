b4b91c7d40a3d2c9df429fe45a31ef77ed815252
Stephen Russett <stephenrussett@gmail.com>
2014-04-21 03:25:00 -0400

    Commented out older date code.  Will likely remove in future

--- github-analytics-analyze-data/issues_date_processor.rb
+++ github-analytics-analyze-data/issues_date_processor.rb
@@ -4,25 +4,25 @@ require_relative 'helpers'
 
 module Issues_Date_Processor
 
-    def self.analyze_issues_date_year_repo(user, repo, filterYear, githubAuthInfo)
-      userRepo = "#{user}/#{repo}"
-      Issues_Date_Aggregation.controller
-      spentHours = Issues_Date_Aggregation.get_repo_time_month_year_sum(userRepo, filterYear, githubAuthInfo)
-      # issues = Helpers.merge_issue_time_and_budget(spentHours, budgetHours)
-      spentHours.each do |x|
-        if x["time_duration_sum"] != nil
-          x["time_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["time_duration_sum"], "long")
-        end
-        # if x["budget_duration_sum"] != nil
-        #   x["budget_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["budget_duration_sum"], "long")
-        # end
-      end
+    # def self.analyze_issues_date_year_repo(user, repo, filterYear, githubAuthInfo)
+    #   userRepo = "#{user}/#{repo}"
+    #   Issues_Date_Aggregation.controller
+    #   spentHours = Issues_Date_Aggregation.get_repo_time_month_year_sum(userRepo, filterYear, githubAuthInfo)
+    #   # issues = Helpers.merge_issue_time_and_budget(spentHours, budgetHours)
+    #   spentHours.each do |x|
+    #     if x["time_duration_sum"] != nil
+    #       x["time_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["time_duration_sum"], "long")
+    #     end
+    #     # if x["budget_duration_sum"] != nil
+    #     #   x["budget_duration_sum_human"] = Helpers.convertSecondsToDurationFormat(x["budget_duration_sum"], "long")
+    #     # end
+    #   end
 
-      # issues = self.process_issues_for_budget_left(issues)
+    #   # issues = self.process_issues_for_budget_left(issues)
 
-      # return issues
-      return spentHours
-    end
+    #   # return issues
+    #   return spentHours
+    # end
 
 
 
